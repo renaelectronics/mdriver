6cb6908db589c176f52a1a63d971e4c2ba274071
Thomas Tai <thomastai@renaelectronics.com>
2015-03-15 17:34:24 -0400

    update
    Signed-off-by: Thomas Tai <thomastai@renaelectronics.com>

--- mtutil/mill.sh
+++ mtutil/mill.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "### Setting driver for milling  machine ###"
-sudo ./motordrv -m 1 -c 1.5 -s 1
-sudo ./motordrv -m 2 -c 1.5 -s 1
-sudo ./motordrv -m 3 -c 1.5 -s 1
+sudo ./mtutil -m 1 -c 1.5 -s 1
+sudo ./mtutil -m 2 -c 1.5 -s 1
+sudo ./mtutil -m 3 -c 1.5 -s 1
